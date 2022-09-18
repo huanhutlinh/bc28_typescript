502599bdbfe56316ecedfd4b5d683ed5bf11cb2d
macbook <khaitruong1301@gmail.com>
2022-09-18 14:30:38 +0700

    ts_react

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,40 +1,44 @@
 //tsrfc
-import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux';
-import Product from '../../components/Product/Product';
-import { AppDispatch, RootState } from '../../redux/configStore';
-import { getProductApi, ProductModel } from '../../redux/reducers/productReducer';
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Product from "../../components/Product/Product";
+import { AppDispatch, RootState } from "../../redux/configStore";
+import {
+  getProductApi,
+  ProductModel,
+} from "../../redux/reducers/productReducer";
 
 type Props = {
-    title?:string;
-}
+  title?: string;
+};
 
-export default function Home({title}: Props) {
-
-  const {arrProduct} = useSelector((state:RootState)=>state.productReducer)
-  const dispatch:AppDispatch = useDispatch();
-  useEffect(()=>{
+export default function Home({ title }: Props) {
+  const { arrProduct } = useSelector(
+    (state: RootState) => state.productReducer
+  );
+  const dispatch: AppDispatch = useDispatch();
+  useEffect(() => {
     //call api = action thunk
     const actionApi = getProductApi();
     dispatch(actionApi);
-
-  },[])
+  }, []);
 
   const renderProduct = () => {
-
-    return arrProduct.map((prod:ProductModel,index:number)=>{
-      return <div className='col-4 mt-2' key={index}>
+    return arrProduct.map((prod: ProductModel, index: number) => {
+      return (
+        <div className="col-4 mt-2" key={index}>
           <Product product={prod} />
-      </div>
-    })
-  }
+        </div>
+      );
+    });
+  };
 
   return (
-    <div className='container'>
-      <h3 className='text-center'>Shoes Shop</h3>
-      <div className='row'>
-        {renderProduct()}
-      </div>
+    <div className="container">
+      <h3 className="text-center">Shoes Shop</h3>
+    
+
+      <div className="row">{renderProduct()}</div>
     </div>
-  )
+  );
 }
